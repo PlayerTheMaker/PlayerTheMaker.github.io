fb954eea0f1b76560099593d53583456058c2819
PlayerTheMaker <kavanabaya@hotmail.com>
2025-07-30 21:31:45 -0700

    Simple Updated Website, old still there

---
+++ OldWebsite/Secret.js
@@ -0,0 +1,15 @@
+time = 0
+
+
+while (time < 100000){
+  //
+  time += 0.05
+}
+
+document.getElementById("hi").innerHTML = "There is a rickroll";
+
+alert("never gonna rickroll you")
+alert("never gonna let you down")
+alert("never going to rick around and roll you")
+
+document.getElementById("hi").innerHTML = "There was a rickroll";
